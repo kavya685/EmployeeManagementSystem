243046ce635a953e9e186b3627c8cc559d38531c
kavya685 <kavyapopat95@gmail.com>
2026-05-24 16:08:14 +0530

    implement the project structure and entity

---
+++ src/main/java/com/kavya/EmployeeManagementSystem/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.kavya.EmployeeManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Column(nullable = false)
+    private Double salary;
+}
